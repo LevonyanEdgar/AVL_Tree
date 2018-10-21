9e46d6f46867a741f3b103e2de82dc070bc9abaa
LevonyanEdgar <edgarlevonyandp@gmail.com>
2018-10-21 18:23:14 +0400

    AVL v0.1

---
+++ AVLTree/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVLTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AVL tree = new AVL();
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+            tree.Add(2);
+            tree.Delete(7);
+            tree.DisplayTree();
+        }
+    }
+}
